8e5ba07d444e177b16864fdbe5d3cb5f29f8f4e3
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-12-21 14:44:38 +0100

    Generated a list of 10 email

--- js/script.js
+++ js/script.js
@@ -3,16 +3,31 @@ const {createApp} = Vue;
 createApp({
 	data() {
 		return{
-          
+          emailList : [],
 		}
 	},
 
     methods: {
-        
+
+        generateRandomEmail(){
+            for (let i = 0; i < 10; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                 .then((response) => {
+                     const result = response.data.response;
+                    // this.emailList.push(result);
+                     console.log(result);
+                     this.emailList.push(result)
+                 });
+                
+            }
+
+        },
+
     },
     
     created(){
-
+        this.generateRandomEmail();
     }
 
 
